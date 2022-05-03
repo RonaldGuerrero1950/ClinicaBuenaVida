11cbde4b7ab227a90c28601c4df7e39e959f19aa
RonaldGuerrero1950 <renriquegduran@gmail.com>
2022-05-02 23:05:41 -0300

    avance

---
+++ js/principal.js
@@ -0,0 +1,36 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for (let index = 0; index < pacientes.length; index++) {
+    var paciente = pacientes[index];
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var tdImc = paciente.querySelector(".info-imc");
+    var pesoEsValido=true;
+    var alturaEsValida=true;
+
+    if((peso<0) || (peso>400)){
+        tdImc.textContent="Peso incorrecto";
+        pesoEsValido=false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if((altura<0) || (altura>3)){
+        tdImc.textContent="Altura incorrecta";
+        alturaEsValida=false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if(pesoEsValido && alturaEsValida){
+        var imc = peso/(altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+    
+}
+
+/* Cómo seleccionar diversos elementos con querySelectorAll().
+Recordamos las iteraciones con ciclo for.
+Manipulación de estilos con la propiedad. style.
+Buenas prácticas al no alterar el estilo directo en JavaScript, sino usar CSS, a través de clases con la propiedad .classList(). */
+
+
+
